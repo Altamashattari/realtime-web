b343717f31c831358d63e4b758b910de74d7424d
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-09-26 23:06:06 +0530

    all changes

---
+++ polling/exercise/frontend/polling-chat.js
@@ -0,0 +1,80 @@
+const chat = document.getElementById("chat");
+const msgs = document.getElementById("msgs");
+
+// let's store all current messages here
+let allChat = [];
+
+// the interval to poll at in milliseconds
+const INTERVAL = 3000;
+
+// a submit listener on the form in the HTML
+chat.addEventListener("submit", function (e) {
+  e.preventDefault();
+  postNewMsg(chat.elements.user.value, chat.elements.text.value);
+  chat.elements.text.value = "";
+});
+
+async function postNewMsg(user, text) {
+  const data = { user, text };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  return fetch("/poll", options);
+}
+
+let failedTries = 0;
+async function getNewMsgs() {
+  let json;
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+
+    if (res.status >= 400) {
+      throw new Error("request did not succeed: " + res.status);
+    }
+    allChat = json.msg;
+    render();
+    failedTries = 0;
+  } catch (err) {
+    // backoff
+    console.error("polling error", e);
+    failedTries++;
+  }
+  /*
+  this will be triggered even if user move away to different tab, thus wasting server resources
+  setTimeout(getNewMsgs, INTERVAL); 
+  */
+}
+
+function render() {
+  // as long as allChat is holding all current messages, this will render them
+  // into the ui. yes, it's inefficent. yes, it's fine for this example
+  const html = allChat.map(({ user, text, time, id }) =>
+    template(user, text, time, id)
+  );
+  msgs.innerHTML = html.join("\n");
+}
+
+// given a user and a msg, it returns an HTML string to render to the UI
+const template = (user, msg) =>
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+
+const BACKOFF = 5000;
+let timeToMakeNextRequest = 0;
+
+// raf - request animation frame
+async function rafTimer(time) {
+  if (timeToMakeNextRequest <= time) {
+    await getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL + failedTries * BACKOFF;
+  }
+  requestAnimationFrame(rafTimer);
+}
+
+// when browser doesnt have any io, it will trigger
+// wont interrupt browser
+requestAnimationFrame(rafTimer);
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Realtime Technologies
+
+- Polling
+
+- HTTP/2 Push
+  - Long Running Process
+
+- Web Sockets
+
+- Socket.IO
+
+<https://bit.ly/intro-realtime>
